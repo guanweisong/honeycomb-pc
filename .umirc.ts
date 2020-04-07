b5c5496adf15434f6f57efb9fd285d043a510848
guanweisong <307761682@qq.com>
2020-04-07 20:28:41 +0800

    cdn配置优化

--- .umirc.ts
+++ .umirc.ts
@@ -14,7 +14,7 @@ const config: IConfig = {
   },
   theme: theme,
   ignoreMomentLocale: true,
-  publicPath: 'https://cdn.guanweisong.com/',
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : 'https://cdn.guanweisong.com/',
   hash: true,
   routes: [
     {
